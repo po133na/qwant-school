1b2ef74cdc34b1c8c693fd528cc5e0bd835ab4ba
Polina S <122814879+po133na@users.noreply.github.com>
2023-01-16 23:00:09 +0600

    my square

---
+++ my_square.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+int my_square(int x, int y){
+    for(int i=0; i<x; i++)
+    {
+        for(int j = 0; j < y; j++)
+        {
+            if(i == 0 || i == x - 1){
+                if(j > 0 && j < y - 1){
+                    printf("-");
+                    continue;
+                }
+                printf("o");
+            }else if(j == 0 || j == y - 1){
+                printf("|");
+            }else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    return 0;
+}
+int main(int ac, char **av){
+    if(ac != 3){
+        return 0;
+    }
+    int x = atoi(av[1]);
+    int y = atoi(av[2]);
+    my_square(x,y);
+} 
